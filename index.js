10631974cf998a8560163a62a36f9f5ff3f96622
Tiarethorns <tcarolinaespinoza@gmail.com>
2022-10-12 19:11:29 -0300

    filterExtension

--- index.js
+++ index.js
@@ -16,29 +16,47 @@ const fixPath = (route) => {
     let fixedPath = path.resolve(route);
     return fixedPath;
   }
-
   return route;
 };
 
-console.log(fixPath("README.md"));
+console.log("la ruta absoluta es " + fixPath("README.md"));
 
 // Verifica si la ruta es un archivo, si es un directorio retorna false
 // Use statSync() method to store the returned
 // instance into variable named stats
 
-const DirOrFile = (route) => {
+const isFile = (route) => {
   let stats = fs.statSync("README.md");
-  console.log("is file ? " + stats.isFile());
+  console.log("Es un archivo ? " + stats.isFile());
   return stats.isFile();
 };
-DirOrFile();
-
-//console.log("is directory ? " + stats.isDirectory());
-
-// comprobar la extension del rchivo
+isFile();
+// comprobar la extension del archivo
 const extension = (route) => {
   console.log("La extension es" + path.extname("README.md"));
   return path.extname("README.md");
 };
 extension();
 
+//mostrar los archivos de un directorio
+let foundFiles = [];
+let filenames = fs.readdirSync("C:\\Users\\dizzy\\SCL021-cipher");
+console.log("Archivos en la carpeta:");
+console.log(filenames);
+
+/*for (i = 0; i < filenames.length; i++) {
+  
+  filenames.push(foundFiles);
+}
+console.log(foundFiles);*/
+
+filenames.forEach((file) => {
+  foundFiles.push(file);
+  //console.log("File:", file);
+  console.log(foundFiles);
+});
+// filtrar solo los que tengan extension .md
+const filterExtension = filenames.filter(
+  (file) => path.extname(file) === ".md"
+);
+console.log(filterExtension);
