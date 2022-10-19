0a11e0d5e2cdf53bdc2f9de252f4413f1d78c3b2
Tiarethorns <tcarolinaespinoza@gmail.com>
2022-10-19 20:41:43 -0300

    pruebas

--- index.js
+++ index.js
@@ -5,59 +5,50 @@ const { resolve, isAbsolute } = require("path");
 const path = require("path");
 const fs = require("fs");
 const https = require("https");
-//const fetch = require("node-fetch");
 const [p1, p2, pathFile, ...args] = process.argv;
-console.log({ pathFile, args });
+//console.log({ pathFile, args });
 
 //comprobar que la ruta sea absoluta y si no, transformarla
 const fixPath = (route) => {
-  let pathIsAbsolute = path.isAbsolute(route);
-
+  let pathIsAbsolute = path.isAbsolute(pathFile);
   if (pathIsAbsolute === false) {
-    let fixedPath = path.resolve(route);
+    let fixedPath = path.resolve(pathFile);
     return fixedPath;
+  } else {
+    return console.log("la ruta ya es absoluta");
   }
-  return route;
 };
+console.log("la ruta absoluta es " + fixPath(pathFile));
 
-console.log("la ruta absoluta es " + fixPath("README.md"));
-
-// Verifica si la ruta es un archivo, si es un directorio retorna false
 const isFile = (route) => {
-  let stats = fs.statSync("README.md");
-  console.log("Es un archivo ? " + stats.isFile());
+  let stats = fs.statSync(pathFile);
   return stats.isFile();
 };
 isFile();
-// comprobar la extension del archivo
-const extension = (route) => {
-  console.log("La extension es" + path.extname("README.md"));
-  return path.extname("README.md");
+
+const isValid = (route) => {
+  let isMD = path.extname(pathFile);
+  console.log(isMD);
+  if (isFile(pathFile) === true && isMD === ".md") {
+    console.log("el archivo es válido");
+  } else {
+    return console.log("Debes inspeccionar un archivo tipo Markdown");
+  }
 };
-extension();
-
-//archivos de un directorio
-let filenames = fs.readdirSync("C:\\Users\\dizzy\\SCL021-cipher");
-console.log("Archivos en la carpeta:");
-console.log(filenames);
-
-// filtrar solo los que tengan extension .md
-const filterExtension = filenames.filter(
-  (file) => path.extname(file) === ".md"
-);
-console.log(filterExtension);
-console.log(filterExtension.length);
+isValid();
+//corroborar si el archivo contiene links
 
 function detectURLs(message) {
   var urlRegex = /(((https?:\/\/)|(www\.))[^\s]+)/g;
   return message.match(urlRegex);
 }
-// leer los archivos
+
 const readFile = (route) => {
   return new Promise((resolve, reject) => {
-    fs.readFile("README.md", "utf8", (err, data) => {
+    fs.readFile(pathFile, "utf8", (err, data) => {
       console.log("lenyendo archivo", typeof data);
       if (err) {
+        return console.log("El archivo no es válido");
         reject(err);
       } else if (data) {
         const urlsDetected = detectURLs(data);
@@ -68,25 +59,3 @@ const readFile = (route) => {
   });
 };
 readFile().then((data) => console.log(data.length));
-//.map transformar arreglo de url a arreglo de promesas
-
-https.get("https://bluuweb.github.io/javascript/02-dom/", (res) => {
-  //https.get(url[, options][, callback])
-  if (res.statusCode === 200) {
-    return console.log(
-      "statusCode:",
-      res.statusCode,
-      "status Message:",
-      res.statusMessage
-    );
-  } else {
-    return console.log(
-      "statusCode:",
-      res.statusCode,
-      "status Message:",
-      res.statusMessage
-    );
-  }
-});
-
-//usar includes para trabajar con validate y stats
